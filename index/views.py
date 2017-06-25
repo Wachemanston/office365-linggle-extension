8b5baf313b5d4b2cd75dfb10f15d491bfa08d3e1
JJC <jjc@nlplab.cc>
2017-06-25 17:45:57 +0800

    inteli-query only when no wildcard is used

--- index/views.py
+++ index/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import requests
+import re
 
 API_URL = 'http://ironman.nlpweb.org:8745?search={}'
 # API_URL = 'http://lost.nlpweb.org:8745?search={}'
 API_URL2 = 'http://lost.nlpweb.org:3440/api/mix/{}'
+linggle_symbol = '_~?*/.'
 
 
 def index(request):
@@ -24,15 +26,16 @@ def emailpro(request):
 
 
 def linggleit(request, query):
-    tokens = query.split()
-    if len(tokens) == 3 and ' of ' in query and query.endswith('ing'):
-        query = query.replace(' of ', ' of/to ').replace(tokens[-1], tokens[-1]+'/'+tokens[-1][:-3])
-    elif len(tokens) == 3 and ' to ' in query and not query.endswith('ing'):
-        query = query.replace(' to ', ' ?to ').replace(tokens[-1], tokens[-1]+'/'+tokens[-1]+'ing')
-    elif len(tokens) == 2 and query.endswith('ing'):
-        query = query.replace(' ', ' ?to ').replace(tokens[-1], tokens[-1]+'/'+tokens[-1][:-3])
-    else:
-        query += ' *'+' _'*(3-query.count(' '))
+    if all(ch not in query for ch in linggle_symbol):
+        tokens = query.split()
+        if len(tokens) == 3 and ' of ' in query and query.endswith('ing'):
+            query = query.replace(' of ', ' of/to ').replace(tokens[-1], tokens[-1]+'/'+tokens[-1][:-3])
+        elif len(tokens) == 3 and ' to ' in query and not query.endswith('ing'):
+            query = query.replace(' to ', ' ?to ').replace(tokens[-1], tokens[-1]+'/'+tokens[-1]+'ing')
+        elif len(tokens) == 2 and query.endswith('ing'):
+            query = query.replace(' ', ' ?to ').replace(tokens[-1], tokens[-1]+'/'+tokens[-1][:-3])
+        else:
+            query += ' *'+' _'*(3-query.count(' '))
     r = requests.get(API_URL.format(query))
     return HttpResponse(content=r.text, status=r.status_code)
 
