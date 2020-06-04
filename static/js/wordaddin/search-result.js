c56d37b48757ac02e43bfe9a546623dc68f2dd63
ArrmenLee <wachemanstonphoenix@gmail.com>
2020-06-04 14:56:16 +0800

    (Template)[Modify] Cooperate with new bacckend api

--- static/js/wordaddin/search-result.js
+++ static/js/wordaddin/search-result.js
@@ -15,6 +15,17 @@ const SearchResult = {
         searchResult.html('');
     },
 
+    segment_digits: (number) => {
+        const segmentSize = 3;
+        const numberToStr = number.toString();
+        const digits = numberToStr.length;
+        let segmentNumber = '';
+        for (let i = 1; i <= digits; i += 1) {
+            segmentNumber = `${!(i % segmentSize) && i !== digits ? ',' : ''}${numberToStr[digits - i]}${segmentNumber}`;
+        }
+        return segmentNumber;
+    },
+
     renderSearchFail: () => {
         const searchResult = $('.linggle.search-result');
         const htmlFrag = '<div class="container">Something goes wrong.' +
@@ -29,22 +40,23 @@ const SearchResult = {
 
     renderSearchResult: (data) => {
         const searchResult = $('.linggle.search-result');
-        let htmlFrag = (data.length > 0) ? '' : '<div class="container"><h3>No result</h3></div>';
-
-        data.forEach((element) => {
-            const ngramstr = element.phrase.join(' ');
-            const percentstr = element.percent;
-            const countstr = element.count_str;
-            const ngramIdstr = element.phrase.join('_');
-            // TODO: template literals is in ES6, which is not compatible with IE11
-            htmlFrag += '<ul class="list-group"><li class="list-group-item">' +
-                '<span class="badge"><h5>' + countstr + '</h5></span>' +
-                '<span>' + percentstr + '</span>' +
-                '<h4>' + ngramstr + '</h4>' +
-                '<div class="progress"><div class="progress-bar" role="progressbar" style="width: ' + percentstr + '"></div>' +
-                '</div></li></ul>';
-        });
-        searchResult.html(htmlFrag);
+        if (Object.keys(data).length === 0) {
+            searchResult.html('<div class="container"><h3>No result</h3></div>');
+        } else {
+            let htmlFrag = '';
+            $.each(data, (ngram, obj) => {
+                const { percent, count: ctn } = obj;
+                const count = SearchResult.segment_digits(ctn);
+                // TODO: template literals is in ES6, which is not compatible with IE11
+                htmlFrag += `<ul class="list-group"><li class="list-group-item">
+                    <span class="badge"><h5>${count}</h5></span>
+                    <span>${percent} %</span>
+                    <h4>${ngram}</h4>
+                    <div class="progress"><div class="progress-bar" role="progressbar" style="width:${percent}%"></div>
+                    </div></li></ul>`;
+            });
+            searchResult.html(htmlFrag);
+        }
     },
 };
 
